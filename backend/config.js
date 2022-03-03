8a77957f7fb8ff2abb1346d66ffa5451a8a0b5b8
Thilin98 <thlini1998@gmail.com>
2022-03-03 14:53:06 +0530

    Connect to database

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// Connection to the mongoDB database(test) 
+let config = {
+    dbUrl:
+      "mongodb+srv://dbUser:dbUser@cluster0.oxsff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
+
+module.exports = config;
