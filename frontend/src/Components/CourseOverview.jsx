ba397edb0353103392ce1c4000952b5840ffaf3b
hashiniWijerathne <e17398@eng.pdn.ac.lk>
2022-03-03 08:58:51 +0530

    add basic statistics to frontend

--- frontend/src/Components/CourseOverview.jsx
+++ frontend/src/Components/CourseOverview.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Users from './Users';
+import LogRecords from './LogRecords'
 
 export default function CourseOverview() {
     return (
         <div>
             <h1>Overview</h1>
+            <LogRecords/>
+            <Users/>
         </div>
     );
 }
