786a64d9016ac7f47220d4c4a64386ebec2671c3
Isara Tillekeratne <e17352@eng.pdn.ac.lk>
2022-03-21 20:17:06 +0530

    plot data for submissions endpoint

--- backend/routes/submissions.js
+++ backend/routes/submissions.js
@@ -3,7 +3,7 @@ const submitted_submission = require("../models/submitted_submission");
 const summary_submission = require("../models/summary_submission");
 const router = express.Router();
 
-
+// temporary
 router.get("/", async (req, res) => {
     try {
         let submitted_submissions = await submitted_submission.find();
@@ -19,12 +19,31 @@ router.get("/", async (req, res) => {
     }
 });
 
+// temporary
 router.get("/summary", async (req, res) => {
     try {
         let summary_submissions = await summary_submission.find();
         res.status(200).json({
             status: 200,
             data: summary_submissions,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
+
+// get plot data for a certain submission
+router.get("/summary/plot", async (req, res) => {
+
+    let subName = req.body.subName;
+    try {
+        let plotData = await summary_submission.find({ Submission_name: subName });
+        res.status(200).json({
+            status: 200,
+            data: plotData,
         });
     } catch (err) {
         res.status(400).json({
