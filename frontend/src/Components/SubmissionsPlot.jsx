bf5b485c1e1a11e83409b8ad8b11c00aa6d8935a
Isara Tillekeratne <e17352@eng.pdn.ac.lk>
2022-03-22 21:31:21 +0530

    attempt to divide x y data

--- frontend/src/Components/SubmissionsPlot.jsx
+++ frontend/src/Components/SubmissionsPlot.jsx
@@ -3,19 +3,33 @@ import { getSubmissionPlotDetails } from '../APIcalls/Submissions';
 
 export default function SubmissionPlot() {
 
-    let [xPoints, setXPoints] = React.useState([]);
-    let [YPoints, setYPoints] = React.useState([]);
+    // let [xPoints, setXPoints] = React.useState([]); // infinite thing
+    // let [YPoints, setYPoints] = React.useState([]);
 
     const setAxisData = () => {
         getSubmissionPlotDetails().then((res) => {
-            console.log(res)
+            // console.log(res.data)
+            let plotDataX = [];
+            let plotDataY = [];
+            res.data.map((obj) => {
+                plotDataX.push(obj.Date);
+                plotDataY.push(obj.Count);
+            })
+            // console.log(plotDataX, plotDataY) // worked
+            return plotDataX, plotDataY;
         })
+    }
+
+    const generatePlot = () => {
+        console.log(setAxisData()) // fix error undefined prints
+        // draw plot from dummy x, y values
     }
 
     return (
         <div>
             <h3>submission plot</h3>
-            {setAxisData()}
+            {/* {setAxisData()} */}
+            {generatePlot()}
         </div>
     );
 }
