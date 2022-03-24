37967855c640f3c25635280c75051a64db740294
Isara Tillekeratne <e17352@eng.pdn.ac.lk>
2022-03-24 23:06:03 +0530

    submission plots chartjs attempt

--- frontend/src/Components/SubmissionsPlot.jsx
+++ frontend/src/Components/SubmissionsPlot.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
 import { getSubmissionPlotDetails } from '../APIcalls/Submissions';
+import BarChart from './BarChart';
+import { useState } from 'react';
 
 export default function SubmissionPlot() {
 
-    // let [xPoints, setXPoints] = React.useState([]); // infinite thing
-    // let [YPoints, setYPoints] = React.useState([]);
+    let plotDataX = ["date1", "date2"]; // dummy values
+    let plotDataY = [1, 2];
 
     const setAxisData = () => {
-        getSubmissionPlotDetails().then((res) => {
-            // console.log(res.data)
-            let plotDataX = [];
-            let plotDataY = [];
+        getSubmissionPlotDetails().then((res) => { // worked
+
             res.data.map((obj) => {
                 plotDataX.push(obj.Date);
                 plotDataY.push(obj.Count);
             })
-            // console.log(plotDataX, plotDataY) // worked
-            return plotDataX, plotDataY;
+
         })
     }
 
-    const generatePlot = () => {
-        console.log(setAxisData()) // fix error undefined prints
-        // draw plot from dummy x, y values
+    const GeneratePlot = () => {
+        console.log(plotDataX, plotDataY)
+        const [data, setData] = useState({
+            labels: plotDataX, // x axis
+            datasets: [
+                {
+                    label: "Users Gained",
+                    data: plotDataY, // y axis
+                    backgroundColor: [
+                        "rgba(75,192,192,1)",
+                        "#ecf0f1",
+                        "#50AF95",
+                        "#f3ba2f",
+                        "#2a71d0",
+                    ],
+                    borderColor: "black",
+                    borderWidth: 2,
+                },
+            ],
+        });
+
+        return (
+            <div style={{ width: 700 }}>
+                <BarChart chartData={data} />
+            </div>
+        );
     }
 
     return (
         <div>
             <h3>submission plot</h3>
-            {/* {setAxisData()} */}
-            {generatePlot()}
+            {setAxisData()}
+            {GeneratePlot()}
+
         </div>
     );
 }
