7971214faaff01e9b2e3692b1efedfed68c3329e
Thilin98 <thlini1998@gmail.com>
2022-03-26 12:18:39 +0530

    Generating backend connections need for learning_material graphs

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,9 @@
 // routes/posts.js
-
+const mongoose = require("mongoose");
 const express = require("express");
-const Post = require("../models/Post");
+const Post = require("../models/Learning_material");
+const User = require("../models/User");
+const System = require("../models/System");
 const router = express.Router();
 
 // Add objects to DB(test)
@@ -23,19 +25,52 @@ router.post("/", async (req, res) => {
 
 // Get list of objects in DB
 router.get("/list", async (req, res) => {
-    try {
-        let posts = await Post.find();
+   try {
+        let posts = await Post.find().limit(1).sort({$natural:-1});
         res.status(200).json({
             status: 200,
             data: posts,
         });
     } catch (err) {
-        res.status(400).json({
+         res.status(400).json({
             status: 400,
             message: err.message,
         });
     }
 });
 
+// Get list of objects in DB
+router.get("/user", async (req, res) => {
+    //var o = mongoose.model('System',Sys)
+    try {
+         let user = await User.find();
+         res.status(200).json({
+             status: 200,
+             data: user,
+         });
+     } catch (err) {
+          res.status(400).json({
+             status: 400,
+             message: err.message,
+         });
+     }
+ });
+
+ // Get list of objects in DB
+router.get("/sys", async (req, res) => {
+    //var o = mongoose.model('System',Sys)
+    try {
+         let sys = await System.find();
+         res.status(200).json({
+             status: 200,
+             data: sys,
+         });
+     } catch (err) {
+          res.status(400).json({
+             status: 400,
+             message: err.message,
+         });
+     }
+ });
 
 module.exports = router;
